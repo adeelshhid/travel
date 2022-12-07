e477c71c5f68c34bfb1a47a32d0e70a4c980b937
Adeel Shahid <60320029+adeelshhid@users.noreply.github.com>
2022-12-07 22:51:11 +0500

    order-detail

--- guest-app-fh-develop/src/app/pages/order-details/order-details.page.ts
+++ guest-app-fh-develop/src/app/pages/order-details/order-details.page.ts
@@ -8,22 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.page.scss'],
 })
 export class OrderDetailsPage implements OnInit {
-  public progress=0;
-  constructor(private global:GlobalService, private route:Router) { 
+  public progress = 0;
+  isConfirmed:boolean = false
+  constructor(private global: GlobalService, private route: Router) {
 
 
-setInterval(() =>{
-  this.progress += 0.001;
-
-  if(this.progress == 0.30){
-this.route.navigate(['/order-details'])
-  }
-})
+   const interval =  setInterval(() => {
+      this.progress += 0.001;
+      if (this.progress >=0.5) {
+        this.progress = 0
+        console.log(this.progress)
+      }
+    })
+    
 
   }
 
   ngOnInit() {
-    
+
+  }
+  progessChange(evt) {
+    console.log(evt)
+    if (this.progress == 0.50) {
+      alert('asdas')
+    }
   }
 
 }
